b7af3ac78f81a22f35bae40ef7f7b43a87ba88dc
liverm0r <arturdumchev@yandex.ru>
2022-04-17 14:25:25 +0300

    tmp

--- lib/cljd-out/minataurus/main.dart
+++ lib/cljd-out/minataurus/main.dart
@@ -4,26 +4,30 @@ import "../cljd/core.dart" as UmIcmlcoc_core;
 import "package:flutter/material.dart" as f_material;
 import "package:flutter/painting.dart" as f_painting;
 import "package:flutter/widgets.dart" as f_widgets;
-import "package:fluro/fluro.dart" as f_fluro;
 import "package:flutter/src/widgets/framework.dart" as fsw_framework;
+import "package:fluro/fluro.dart" as f_fluro;
 
 // BEGIN main
 dc.dynamic main(){
-return f_material.runApp(f_material.MaterialApp(title: "Welcome to Flutter", theme: f_material.ThemeData(primarySwatch: f_material.Colors.pink, ), home: (UmIcmlcom_main.scaffold() as fsw_framework.Widget?), ), );
+final dc.List<dc.dynamic> fl$1=(dc.List<dc.dynamic>.filled(2, "/", ));
+fl$1[1]=UmIcmlcom_main.main_page;
+final UmIcmlcoc_core.PersistentHashMap $1=UmIcmlcoc_core.$_map_lit(fl$1, );
+final dc.Map<dc.String, fsw_framework.Widget Function(fsw_framework.BuildContext, )> casted$1=($1.cast<dc.String, fsw_framework.Widget Function(fsw_framework.BuildContext, )>());
+return f_material.runApp(f_material.MaterialApp(title: "Welcome to Flutter", theme: f_material.ThemeData(primarySwatch: f_material.Colors.pink, ), initialRoute: "/", routes: casted$1, ), );
 }
 
 // END main
 
-// BEGIN register
-dc.dynamic register(){
-return (UmIcmlcom_main.router.define(const UmIcmlcoc_core.Keyword(null, "handler", 297822107, ), UmIcmlcom_main.root_handler, ));
+// BEGIN main-page
+fsw_framework.Widget main_page(dc.dynamic ctx$1, ){
+return f_material.Scaffold(appBar: f_material.AppBar(title: f_widgets.Text("clojure dart", ), ), body: f_widgets.Center(child: f_widgets.Text("This text is Centered!", style: f_painting.TextStyle(color: f_material.Colors.red, fontSize: 32.0, ), ), ), );
 }
 
-// END register
+// END main-page
 
 // BEGIN root-handler
 var root_handler=f_fluro.Handler(type: f_fluro.HandlerType.route, handlerFunc: (dc.dynamic contex$1, dc.dynamic params$1, ){
-return UmIcmlcom_main.scaffold();
+return UmIcmlcom_main.main_page();
 }, );
 
 // END root-handler
@@ -32,10 +36,3 @@ return UmIcmlcom_main.scaffold();
 var router=f_fluro.FluroRouter();
 
 // END router
-
-// BEGIN scaffold
-dc.dynamic scaffold(){
-return f_material.Scaffold(appBar: f_material.AppBar(title: f_widgets.Text("Welcome to ClojureDart", ), ), body: f_widgets.Center(child: f_widgets.Text("This text is Centered.", style: f_painting.TextStyle(color: f_material.Colors.red, fontSize: 32.0, ), ), ), );
-}
-
-// END scaffold
